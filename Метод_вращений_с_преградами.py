06c37218779c2dbfa65d79c365376dd48eb45e46
mirolyubovaalyona <niroliubova.aa@students.dvfu.ru>
2021-06-15 00:55:01 +1000

    ахуеть

--- Метод_вращений_с_преградами.py
+++ Метод_вращений_с_преградами.py
@@ -16,60 +16,62 @@ a=np.array(a)
 b=[3, 5, 3]
 n=len(b)
 
-def Jacobi(A):
-    n     = A.shape[0]            # matrix size #columns = #lines
-    maxit = 100                   # maximum number of iterations
-    eps   = 1.0e-15               # accuracy goal
-    pi    = np.pi        
-    info  = 0                     # return flag
-    ev    = np.zeros(n,float)     # initialize eigenvalues
-    U     = np.zeros((n,n),float) # initialize eigenvector
-    for i in range(0,n): U[i,i] = 1.0
-
-    for t in range(0,maxit):
-         s = 0;    # compute sum of off-diagonal elements in A(i,j)
-         for i in range(0,n): s = s + np.sum(np.abs(A[i,(i+1):n]))
-         if (s < eps): # diagonal form reached
-              info = t
-              for i in range(0,n):ev[i] = A[i,i]
-              break
-         else:
-              limit = s/(n*(n-1)/2.0)       # average value of off-diagonal elements
-              for i in range(0,n-1):       # loop over lines of matrix
-                   for j in range(i+1,n):  # loop over columns of matrix
-                       if (np.abs(A[i,j]) > limit):      # determine (ij) such that |A(i,j)| larger than average 
-                                                         # value of off-diagonal elements
-                           denom = A[i,i] - A[j,j]       # denominator of Eq. (3.61)
-                           if (np.abs(denom) < eps): phi = pi/4         # Eq. (3.62)
-                           else: phi = 0.5*np.arctan(2.0*A[i,j]/denom)  # Eq. (3.61)
-                           si = np.sin(phi)
-                           co = np.cos(phi)
-                           for k in range(i+1,j):
-                               store  = A[i,k]
-                               A[i,k] = A[i,k]*co + A[k,j]*si  # Eq. (3.56) 
-                               A[k,j] = A[k,j]*co - store *si  # Eq. (3.57) 
-                           for k in range(j+1,n):
-                               store  = A[i,k]
-                               A[i,k] = A[i,k]*co + A[j,k]*si  # Eq. (3.56) 
-                               A[j,k] = A[j,k]*co - store *si  # Eq. (3.57) 
-                           for k in range(0,i):
-                               store  = A[k,i]
-                               A[k,i] = A[k,i]*co + A[k,j]*si
-                               A[k,j] = A[k,j]*co - store *si
-                           store = A[i,i]
-                           A[i,i] = A[i,i]*co*co + 2.0*A[i,j]*co*si +A[j,j]*si*si  # Eq. (3.58)
-                           A[j,j] = A[j,j]*co*co - 2.0*A[i,j]*co*si +store *si*si  # Eq. (3.59)
-                           A[i,j] = 0.0                                            # Eq. (3.60)
-                           for k in range(0,n):
-                                store  = U[k,j]
-                                U[k,j] = U[k,j]*co - U[k,i]*si  # Eq. (3.66)
-                                U[k,i] = U[k,i]*co + store *si  # Eq. (3.67)
-         info = -t # in case no convergence is reached set info to a negative value "-t"
-    return ev,U,t
+def jacobi(ain,tol = 1.0e-9): # Jacobi method
+ 
+    def maxElem(a): # Find largest off-diag. element a[k,l]
+        n = len(a)
+        aMax = 0.0
+        for i in range(n-1):
+            for j in range(i+1,n):
+                if abs(a[i,j]) >= aMax:
+                    aMax = abs(a[i,j])
+                    k = i; l = j
+        return aMax,k,l
+ 
+    def rotate(a,p,k,l): # Rotate to make a[k,l] = 0
+        n = len(a)
+        aDiff = a[l,l] - a[k,k]
+        if abs(a[k,l]) < abs(aDiff)*1.0e-36: t = a[k,l]/aDiff
+        else:
+            phi = aDiff/(2.0*a[k,l])
+            t = 1.0/(abs(phi) + sqrt(phi**2 + 1.0))
+            if phi < 0.0: t = -t
+        c = 1.0/sqrt(t**2 + 1.0); s = t*c
+        tau = s/(1.0 + c)
+        temp = a[k,l]
+        a[k,l] = 0.0
+        a[k,k] = a[k,k] - t*temp
+        a[l,l] = a[l,l] + t*temp
+        for i in range(k):      # Case of i < k
+            temp = a[i,k]
+            a[i,k] = temp - s*(a[i,l] + tau*temp)
+            a[i,l] = a[i,l] + s*(temp - tau*a[i,l])
+        for i in range(k+1,l):  # Case of k < i < l
+            temp = a[k,i]
+            a[k,i] = temp - s*(a[i,l] + tau*a[k,i])
+            a[i,l] = a[i,l] + s*(temp - tau*a[i,l])
+        for i in range(l+1,n):  # Case of i > l
+            temp = a[k,i]
+            a[k,i] = temp - s*(a[l,i] + tau*temp)
+            a[l,i] = a[l,i] + s*(temp - tau*a[l,i])
+        for i in range(n):      # Update transformation matrix
+            temp = p[i,k]
+            p[i,k] = temp - s*(p[i,l] + tau*p[i,k])
+            p[i,l] = p[i,l] + s*(temp - tau*p[i,l])
+ 
+    a=np.copy(ain)
+    n = len(a)
+    maxRot = 5*(n**2)       # Set limit on number of rotations
+    p = identity(n)*1.0     # Initialize transformation matrix
+    for i in range(maxRot): # Jacobi rotation loop 
+        aMax,k,l = maxElem(a)
+        if aMax < tol: return diagonal(a),p
+        rotate(a,p,k,l)
+    
 
 xx=np.linalg.solve(a, b)
 
-ev,U,t=Jacobi(a)
+ev,U=jacobi(a)
 x=[0]*n
 print('Теперь известные элементы: ')
 for i in range (n):
